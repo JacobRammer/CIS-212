6625144df55338e8e16734d8b231285d80d83be2
Jacob Rammer <jacobrammer@Jacobs-MBP.hsd1.or.comcast.net>
2019-10-05 21:29:11 -0700

    test

--- Project2/project2A.c
+++ Project2/project2A.c
@@ -8,18 +8,27 @@ Project 2
 int main() {
     int a, b;
     int target = 50;
+    int max_val = 2;
 
     for (a = 2; a <= target; a++) {
         for (b = 2; b < a; b++) {
             if (a * b > target) {
+                /*
+                    It's probably very easy to exclude everything less than 10
+                    rather than using this if overly chained if statement, 
+                    but I can't think of anything currently.
+                */
                 break;
             }
-            if ((a * b % b) == 0){
-                if ((a * b % 2) == 1) {
+            if ((a * b % b) == 0)
+            {
+                if ((a * b % 2) == 1)
+                {
                     printf("\n%d is a composite number", (a * b));
                 }
             }
         }
-    }
-  
+
+            // max_val ++;
+        }
 }
