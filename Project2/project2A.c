b733b037e0ced69d5eaf20cb82050ea7548c09f5
Jacob Rammer <jacobrammer@Jacobs-MBP.hsd1.or.comcast.net>
2019-10-06 10:57:36 -0700

    Finished 2a

--- Project2/project2A.c
+++ Project2/project2A.c
@@ -1,60 +1,30 @@
 /*
 Jacob Rammer 
-Project 2
+Project 2a
 */
 
 #include <stdio.h>
 
 int main() {
-    int a, b;
-    int target = 50;
-    int max_val = 2;
+    int value, div_one, div_two;
+    int max_val = 500;
+    // int max_val = 2;
 
-    for (a = 10; a <= target; a++) 
+    for (value = 15; value <= max_val; value++) 
     {
-        
-
+        for (div_one = 1; div_one <= value; div_one++)
+        {
+            for (div_two = 2; div_two <= div_one; div_two++)
+            {
+                if (div_one * div_two == value) 
+                {
+                    if (value % 2 == 1) // if it's odd
+                    {
+                        printf("%d is a composite number.\n", value);
+                        value ++;
+                    }
+                }
+            }
+        }
     }
 }
-
-// #include <stdio.h>
-
-// int main()
-// {
-//     int prime_num, divisor; /* prime_num = the number we are checking to see if prime or not, 
-// 		  		 * and divisor = the number it is trying to be divisable by.
-// 		   		 */
-
-//     for (prime_num = 5; prime_num <= 100; prime_num++)
-//     {
-
-//         /* this prime_num++ means that it is incrementing a in steps of 1
-// 		so it will be counting up an integer each time to go up to 100
-// 		*/
-
-//         for (divisor = 2; divisor <= prime_num; divisor++)
-//         {
-//             /* this will have the divisor start at 2 and increment up and it 
-// 			 *divides against the prime_num to see if there is a remainder or not 
-// 			 */
-
-//             if (prime_num % divisor == 0) /* if this is true then that means
-// 						  * they are divisable and prime_num variable is NOT a prime number
-// 						  */
-//             {
-//                 break; /* Gets us out of the inner for loop and not the outer one */
-//             }
-//         }
-
-//         if (prime_num == divisor)
-//         {
-//             /*this is after that division for loop so if all of those went through 
-// 			 *so this checks that if the two numbers are equal, and the remainder 
-// 			 *was not equal to 0, then they are divisable and prime_num is a prime number.
-// 			 */
-
-//             printf("%d is a prime number\n", prime_num);
-//         }
-//     }
-//     return 0;
-// }
