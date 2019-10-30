c592749b09625827b9d24ab642d81f4b3bf10e26
Jacob Rammer <jacobrammer@Jacobs-MBP.hsd1.or.comcast.net>
2019-10-29 21:41:07 -0700

    project 2e

--- Project2/e/project2E.c
+++ Project2/e/project2E.c
@@ -10,6 +10,9 @@ Project 2e
 int main (int argc, char *argv[])
 {
     int arguments = 0; // will be used to count how mnay words we're finding
+    int counter = 0;  // will check to see if matched charaters are same length
+    int matchingWords = 0;  // Matched words with argv
+    int matchedChars = 0; //  the number of matched chars. will compart to strlen
 
     if (argc < 2)  // file name and at least 1 word not provided
     {
@@ -20,7 +23,7 @@ int main (int argc, char *argv[])
     FILE *fName = fopen(argv[1], "r");
     if (fName == NULL)  // error catch on invalid file
     {
-        printf("<\"%s\"> is not a valid file", argv[1]);
+        printf("\"<%s>\" is not a valid file", argv[1]);
         exit(EXIT_FAILURE);
     }
 
@@ -49,6 +52,33 @@ int main (int argc, char *argv[])
             printf("Strings test and %s are the same\n", argv[k]);
             printf("test:%c\n", argv[k][0]);  // will print the first character of argv
     }
+
+    printf("len of argv[2] is: %lu\n", strlen(argv[2]));
+
+    char test1[5] = "test";
+    for (int q = 0; q != numBytes; q++)
+    {
+        char tempvar1 = buff[q];
+        char tempvar2 = argv[2][q];
+        int tempvar3 = strlen(argv[2]) - 1;
+        if (test1[q] == argv[2][q])
+        {
+            printf("Matched char %c to %c.\n", buff[q], argv[2][q]);
+            matchedChars ++;
+        }else
+        {
+            printf("Strings don't match. Test is: %s, argv is: %s\n", test1, argv[2]);
+            matchedChars = 0;
+            counter = 0;
+        }
+        if (buff[q] == ' '&& matchedChars == strlen(argv[2]) - 1)
+        {
+            matchingWords ++;
+            printf("matching words: %d\n", matchingWords);
+        }
+    }
+
+    // printf("the strlen of buff is: %d\n", numBytes);
 
 
     free(buff);
