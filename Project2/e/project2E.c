5050e3af874bd00d5f09733a60c1c622179add96
Jacob Rammer <jacobrammer@Jacobs-MBP.hsd1.or.comcast.net>
2019-10-30 18:51:20 -0700

    revert commiy

--- Project2/e/project2E.c
+++ Project2/e/project2E.c
@@ -5,18 +5,21 @@
 /*
 Jacob Rammer
 Project 2e
+Please excuse the mess, i thought this was due
+Thursday, not Wednesday
 */
 
 int main (int argc, char *argv[])
 {
     int arguments = 0; // will be used to count how mnay words we're finding
-    int counter = 0;  // will check to see if matched charaters are same length
     int matchingWords = 0;  // Matched words with argv
     int matchedChars = 0; //  the number of matched chars. will compart to strlen
+    int numArgs = argc - 2;
+    int argIndex = 0;  // argv[0][argIndex]
 
-    if (argc < 2)  // file name and at least 1 word not provided
+    if (argc < 3)  // file name and at least 1 word not provided
     {
-        printf("File and word(s) not provided");
+        printf("File and or word(s) not provided");
         exit(EXIT_FAILURE);
     }
 
@@ -29,7 +32,7 @@ int main (int argc, char *argv[])
 
     fseek(fName, 0, SEEK_END);
     int numBytes = ftell(fName);
-    printf("Number of bytes in fName is %d\n", numBytes);
+    // printf("Number of bytes in fName is %d\n", numBytes);
     unsigned char *buff = malloc(numBytes);
     fseek(fName, 0, SEEK_SET);
     fread(buff, sizeof(unsigned char), numBytes, fName);
@@ -43,44 +46,75 @@ int main (int argc, char *argv[])
 
     arguments = argc - 1;
     char * args;
-    printf("# of of arguments is: %d\n", arguments);
-    for (int k = 2; k <= arguments; k++)
-    {
-        // printf("argv is : %s\n", argv[k]);
-        int comp = strcmp("test", argv[k]);
-        if (comp == 0)
-            // printf("Strings test and %s are the same\n", argv[k]);
-            printf("test:%c\n", argv[k][0]);  // will print the first character of argv
-    }
+    // printf("# of arguments is: %d\n", arguments);
+    // for (int k = 2; k <= arguments; k++)
+    // {
+    //     printf("argv is : %s\n", argv[k]);
+    //     int comp = strcmp("test", argv[k]);
+    //     if (comp == 0)
+    //         // printf("Strings test and %s are the same\n", argv[k]);
+    //         printf("test:%c\n", argv[k][0]);  // will print the first character of argv
+    // }
 
+/*
+need to have arg[v] index logic
+*/
     // printf("len of argv[2] is: %lu\n", strlen(argv[2]));
-
-    char test1[5] = "test";
-    for (int q = 0; q != numBytes; q++)
+    int tempvar3 = strlen(argv[2]);
+    // printf("tempvar3 is: %d, numbytes is: %d\n", tempvar3, numBytes);
+    // printf("argv[2][0] is: %c\n", argv[2][0]);
+    for (int args = 2; args < argc; args++)
     {
-        char tempvar1 = buff[q];
-        char tempvar2 = argv[2][q];
-        int tempvar3 = strlen(argv[2]) - 1;
-        if (test1[q] == argv[2][q])
+        argIndex = 0;
+        int maxArgIndex = strlen(argv[args]);
+        // printf("arg lengh is: %d\n", maxArgIndex);
+        for (int q = 0; q < numBytes; q++)
         {
-            printf("Matched char %c to %c.\n", buff[q], argv[2][q]);
-            matchedChars ++;
-        }else
-        {
-            // printf("Strings don't match. Test is: %s, argv is: %s\n", test1, argv[2]);
-            matchedChars = 0;
-            counter = 0;
+            // printf("buff q is:%c, argv is %c\n", buff[q], argv[args][argIndex]);
+            if (argIndex > maxArgIndex)
+                argIndex = 0;
+            if (buff[q] == argv[args][argIndex])
+            {
+                // printf("Matched char %c to %c.\n", buff[q], argv[args][argIndex]);
+                matchedChars ++;
+                argIndex ++;
+                // printf("matched number of chars is: %d\n", matchedChars);
+                if ((matchedChars == tempvar3) && (buff[q + 1] == ' ' || buff[q + 1] == '\0' || buff[q + 1] == '\n' || buff[q + 1] == '.'))
+                {
+                    if (buff[q - 1] != ' ' || buff[q - 1] != '\0' || buff[q - 1] != '\n' || buff[q - 1] != '.')
+                    {
+                        printf("Matched char %c to %c\n", buff[q], argv[args][argIndex]);
+                        printf("matched number of chars is: %d\n", matchedChars);
+                        matchingWords++;
+                        argIndex = 0;
+                        matchedChars = 0;
+                    }
+                }
+                
+            }
+                argIndex=0;
+            }
+            
+            // if ((matchedChars == tempvar3) && (buff[q+1] == ' ' || buff[q+1] == '\0'|| buff[q+1] == '\n' || buff[q+1] == '.'))
+            // {
+            //     printf("Matched char %c to %c\n", buff[q], argv[args][argIndex]);
+            //     printf("matched number of chars is: %d\n", matchedChars);
+            //     matchingWords++;
+            //     argIndex = 0;
+            //     matchedChars = 0;
+            // }
+            if (buff[q] == ' ' || buff[q+1] == '\n' || buff[q+1] == '\0' || buff[q] == '\n')
+            {
+                matchedChars = 0;
+                argIndex = 0;
+            }
         }
-        if (buff[q] == '\n'&& matchedChars == 4)
-        {
-            matchingWords ++;
-            printf("matching words: %d\n", matchingWords);
-        }
+        printf("The word \"%s\" occurs %d times.\n", argv[args], matchingWords);
     }
 
     // printf("the strlen of buff is: %d\n", numBytes);
 
-
+    printf("matching words: %d\n", matchingWords);
     free(buff);
     return 0;
 }
