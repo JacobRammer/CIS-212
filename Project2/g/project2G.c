889f47d064679ceb37a1ac37eda7b599d01a84ed
Jacob Rammer <jacobrammer@Jacobs-MBP.hsd1.or.comcast.net>
2019-11-10 19:59:50 -0800

    fixing 2g to use malloc instead of read from disk

--- Project2/g/project2G.c
+++ Project2/g/project2G.c
@@ -6,11 +6,23 @@ Jacob Rammer
 Project 2g
 */
 
+int readRow(int index, int *buff, FILE *txtFile)
+{
+    for(int i = 0; i < 4; i++)
+    {
+        fprintf(txtFile, "%d\n", buff[index]);
+        index ++;
+    }
+
+    return 0;
+}
+
+
 int main(int argc, char *argv[])
 {
-    int r = 0;
-    int seekIndex = 0;
-    if (argc < 3)
+    int *buff = malloc(sizeof(int) * 48);  // don't need to read the whole thing
+   int index = 0;
+   if (argc < 3)
     {
         printf("File(s) not provided.\n");
         exit(EXIT_FAILURE);
@@ -23,18 +35,15 @@ int main(int argc, char *argv[])
     }
 
     FILE *txtFile = fopen(argv[2], "w+");
-
-    while (seekIndex <= 36)
+    fread(buff, sizeof(int), 49, binFile);
+    
+    for(int i = 0; i < 4; i++)
     {
-        for (int i = 0; i < 4; i++)
-        {
-            fread(&r, sizeof(int), 1, binFile);
-            fprintf(txtFile, "%d\n", r);
-        }
-        seekIndex += 12;
-        fseek(binFile, sizeof(int) * seekIndex, SEEK_SET);
+        readRow(index, buff, txtFile);
+        index += 12;
     }
 
+    free(buff);
     fclose(txtFile);
     fclose(binFile);
     return 0;
