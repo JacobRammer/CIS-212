c3d0fdbe8107742f096e633b2a1d252cbe671535
Jacob Rammer <jacobrammer@Jacobs-MBP.hsd1.or.comcast.net>
2019-11-18 19:08:29 -0800

    removed extra 9

--- Project1/c/proj1c.sh
+++ Project1/c/proj1c.sh
@@ -4,7 +4,7 @@ numLines=$(cat passwd_212 | wc -l)
 currentLine=1
 ((numLines + 1))
 readUpTo=$( grep -n ":9" passwd_212 | cut -d":" -f1 | head -n 1 )
-lastRead=$( grep -n ":99" passwd_212 | cut -d":" -f1 | tail -n 1 )
+lastRead=$( grep -n ":9" passwd_212 | cut -d":" -f1 | tail -n 1 )
 # echo $((lastRead - readUpTo))
 # echo $readUpTo
 cat passwd_212 | head -n $((readUpTo - 1))  >> passwd_212_no90s
