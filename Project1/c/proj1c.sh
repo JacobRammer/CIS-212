364a1439559271fc764364ff668cde78733f9b97
Jacob Rammer <jacobrammer@Jacobs-MBP.hsd1.or.comcast.net>
2019-11-18 18:55:15 -0800

    1c

---
+++ Project1/c/proj1c.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+numLines=$(cat passwd_212 | wc -l)
+currentLine=1
+((numLines + 1))
+readUpTo=$( grep -n ":9" passwd_212 | cut -d":" -f1 | head -n 1 )
+lastRead=$( grep -n ":99" passwd_212 | cut -d":" -f1 | tail -n 1 )
+# echo $((lastRead - readUpTo))
+# echo $readUpTo
+cat passwd_212 | head -n $((readUpTo - 1))  >> passwd_212_no90s
+cat passwd_212 | tail -n $((numLines - lastRead)) >> passwd_212_no90s
+
+cat passwd_212_no90s | cut -d":" -f7 | grep -v '#' | sort | uniq -c| grep /
+
+
+
+
+
+
+
+
+
+
+# echo $lastRead
+# echo $firstExclude
+# echo $numLines
+# while ((currentLine <=numLines)) ; do
+#     lineString=$(sed -n "${currentLine}"p passwd_212)
+#     # echo $lineString
+#     # echo $currentLine
+#     # echo $lineString
+
+#     ((currentLine += 1))
+
+# done
---
+++ lab/1113.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+typedef struct name FullName;
+struct name
+{
+    char *fname;
+    char *lname
+}name;
+
+typedef struct info Info;
+struct info
+{
+    int age;
+    FullName *full_name;
+    void (*print)(Info *);
+    void (*destroy)(Info *);
+};
+
+
+void printName(Info *info)
+{
+    printf("%s\n%s\n%d\n", info->full_name->fname, info->full_name->lname, info->age);
+}
+
+void destroyInfo(Info * info)
+{
+    free(info->full_name->fname);
+    free(info->full_name->lname);
+    free(info->full_name);
+    free(info);
+}
+
+int main()
+{
+    // name myName;
+    FullName *name = (FullName*)malloc(sizeof(FullName));
+    name->fname = strdup("John");
+    name->lname = strdup("Doe");
+
+    Info *info = (Info *)malloc(sizeof(Info));
+    info->age = 30;
+    info->full_name = name;
+    info->print = printName;
+    info->destroy = destroyInfo;
+
+    info->print(info);
+    info->destroy(info);
+
+
+
+    // myName->age = 30;
+    // strcpy(myName->fname, "John");
+    // strcpy(myName->lname, "Doe");
+    // printName(myName);
+}
