8695d65038f8dae8a01ef991b2c24152a29bb1ef
jacobrammer <jrammer101@gmail.com>
2019-12-03 18:59:55 -0800

    project 3c

--- Project3/c/proj3C.c
+++ Project3/c/proj3C.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define QUEUE_SIZE 10
+#define QUEUE_SIZE 30 // change back to 10
 
 typedef struct
 {
@@ -21,7 +21,10 @@ void InitializeQueue(Queue *q)
 void Enqueue(Queue *q, char* val)
 {
     if (q->back + 1 >= QUEUE_SIZE)
+    {
+        printf("Enqueue failed\n");
         exit(EXIT_FAILURE);
+    }
     q->elements[q->back] = val;
     q->back++;
 }
@@ -29,7 +32,10 @@ void Enqueue(Queue *q, char* val)
 char* Dequeue(Queue *q)
 {
     if (q->front < 0)
+    {
+        printf("Failed here\n");
         exit(EXIT_FAILURE);
+    }
     char* beforeDequeue = q->elements[q->front];
     q->front++;
     return beforeDequeue;
@@ -90,8 +96,79 @@ char *NewString(char *s)
     rv[len-1] = '\0'; /* take out newline */
     return rv;
 }
+char* removeLabels(char* name, int index)
+{
+    int newStrLen = strlen(name) - 4;
+    char* newStr = malloc(newStrLen);
+    int newStrIdx = 0;
+    for(int i = index; i < strlen(name); i++)
+    {
+        newStr[newStrIdx] = name[i];
+        newStrIdx++;
+    }
+    char * tmp = newStr;
+//    free(newStr);
+    return tmp;
+}
+void placeInCorrectQueue(char* line, Queue *md, Queue *mr, Queue *fd, Queue *fr, Queue *h)
+{
+    if (line[0] == 'R'  && line[2] == 'F')  // recipient
+    {
+        char* frNoLabel = removeLabels(line, 4);
+//        printf("placed %s in fr queue\n", frNoLabel);
+        Enqueue(fr, frNoLabel);
+    }
+    if (line[0] == 'D'  && line[2] == 'F')  // donor
+    {
+        char* fdNoLabels = removeLabels(line, 4);
+//        printf("placed %s in fd queue\n", fdNoLabels);
+        Enqueue(fd, fdNoLabels);
+    }
+    if (line[0] == 'R'  && line[2] == 'M')
+    {
+        char* mrNoLabels = removeLabels(line, 4);
+//        printf("placed %s in mr queue\n", mrNoLabels);
+        Enqueue(mr, mrNoLabels);
+    }
+    if (line[0] == 'D'  && line[2] == 'M')
+    {
+        char* mdNoLabels = removeLabels(line, 4);
+//        printf("placed %s in md queue\n", mdNoLabels);
+        Enqueue(md, mdNoLabels);
+    }
+    if (line[0] == 'H')
+    {
+        char* hNoLabels = removeLabels(line, 2);
+//        printf("placed %s in h queue\n", hNoLabels);
+        Enqueue(h, hNoLabels);
+    }
+}
 
+void dequeInOrder(char* line, Queue *md, Queue *mr, Queue *fd, Queue *fr, Queue *h)
+{
+    if(line[2] == 'F' && line[0] == 'R')  // deque females
+    {
+//        char* noLabels = removeLabels(line, 4);
+        if((h->front) == (h->back) || (fd->front) == (fd->back)) {
+//            printf("Hospital queue is empty\n");
+            return;
+        }else{
+            printf("MATCH: %s donates to %s at hospital %s\n", Dequeue(fd), Dequeue(fr), Dequeue(h));
+        }
+    }
 
+    if(line[2] == 'M' && line[0] == 'R')  // deque males
+    {
+//        char* noLabels = removeLabels(line, 4);
+        if((h->front) == (h->back) || (md->front) == (md->back)) {
+//            printf("Hospital queue is empty\n");
+            return;
+        }else{
+            printf("MATCH: %s donates to %s at hospital %s\n", Dequeue(md), Dequeue(mr), Dequeue(h));
+        }
+    }
+
+}
 
 int main(int argc, char *argv[])
 {
@@ -108,12 +185,28 @@ int main(int argc, char *argv[])
 //    printf("Dequeue: %s\n", Dequeue(&q));
 //    PrintQueue(&q);
 
+
+    char line[256];  //array that holds each line
+    int lineNum = 0;  // the present line in the file stream
+    int setLines = 0;  // will be the total number of lines in the file
+
+    Queue maleDonors;
+    Queue maleRecipients;
+    Queue femaleDonors;
+    Queue femaleRecipients;
+    Queue hospital;
+    InitializeQueue(&maleDonors);
+    InitializeQueue(&maleRecipients);
+    InitializeQueue(&femaleDonors);
+    InitializeQueue(&femaleRecipients);
+    InitializeQueue(&hospital);
+
+
     if (argc < 2)
     {
         printf("Invalid number of inputs.\n");
         exit(EXIT_FAILURE);
     }
-    printf("argv1 is %s\n", argv[1]);
 
     FILE *donorList = fopen(argv[1], "r");
     if (donorList == NULL)
@@ -121,18 +214,38 @@ int main(int argc, char *argv[])
        printf("%s is not a valid file", argv[1]);
        exit(EXIT_FAILURE);
     }
+    /*
+     Dynamically find number of lines
+     */
+    while(fgets(line, 256, donorList))
+        setLines++;
+    char *buff[setLines];  // you have to set this after the loop ya derp
 
-    fseek(donorList, 0, SEEK_END);
-    long numBytes = ftell(donorList);
-    char *buff = malloc(numBytes);
+
     fseek(donorList, 0, SEEK_SET);
-    fread(buff, sizeof(
-            unsigned char), numBytes, donorList);
-    fclose(donorList);
 
-    char *testTwo = NewString(buff);
+    while(fgets(line, 256, donorList))
+    {
+        buff[lineNum] = NewString(line);
+//        printf("buff[%d] is %s\n", lineNum, buff[lineNum]);
+        lineNum++;
+    }
 
-    printf("buff is: %s\n", testTwo);
+    /*
+     * Start queue
+     */
+    for (int i = 0; i < setLines; i++)
+        placeInCorrectQueue(buff[i], &maleDonors, &maleRecipients, &femaleDonors, &femaleRecipients, &hospital);
+
+
+   for(int i = 0; i < setLines; i++)
+       dequeInOrder(buff[i], &maleDonors, &maleRecipients, &femaleDonors, &femaleRecipients, &hospital);
+//
+//    PrintQueue(&maleDonors);
+//    PrintQueue(&maleRecipients);
+//    PrintQueue(&femaleDonors);
+//    PrintQueue(&femaleRecipients);
+//    PrintQueue(&hospital);
 
 /* Gives this output (with different pointers):
 Printing queue 0x7fff5e6878a8
@@ -172,8 +285,8 @@ Printing queue 0x7fff5e6878a8
  *
     PrettyPrintQueue(&female_donors, "female donors");
     PrettyPrintQueue(&female_recipients, "female recipients");
-    PrettyPrintQueue(&male_donors, "male donors");
-    PrettyPrintQueue(&male_recipients, "male recipients");
+    PrettyPrintQueue(&male_donors, "maleDonors donors");
+    PrettyPrintQueue(&male_recipients, "maleDonors recipients");
     PrettyPrintQueue(&hospitals, "hospitals");
  *
  */
